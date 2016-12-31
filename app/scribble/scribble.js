757314521685fb9816650f6e09972fd796c7520e
Preetam <preetamvp@hotmail.com>
2016-12-31 23:02:57 +1100

    canvas

--- app/scribble/scribble.js
+++ app/scribble/scribble.js
@@ -1,16 +1,78 @@
 'use strict'
 
 let scribble = (function (window) {
-  let canvas = null,
-  context = null,
-  document = window.document
-
+  let { ipcRenderer } = require("electron")
+  let canvas,
+  container,
+  context,
+  canvasWidth,
+  canvasHeight,
+  curX = 0, curY = 0, prevX = 0, prevY = 0,
+  flag = false, dot_flag = false,
+  foreColor = "red", thickness = 2,
+  document = window.document;
 
   function init() {
-    console.log("init")
+    initCanvas();
+    initIpc()
+  }
+
+  function initIpc() {
+    ipcRenderer.on("clear", () => {
+      if(context) {
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        ipcRenderer.send("clear-done")
+      }
+    }) 
+  }
+
+  function initCanvas() {
     canvas = document.getElementById("canvas")
-    context = canvas.getContext("2d")
-    console.log(canvas, context)
+    canvas.width = document.body.clientWidth
+    canvas.height = document.body.clientHeight 
+    context = canvas.getContext("2d");
+
+    canvas.addEventListener('mousemove', (e) => findxy(e), false);
+    canvas.addEventListener('mouseup', (e) => findxy(e), false);
+    canvas.addEventListener('mousedown', (e) => findxy(e), false);
+    canvas.addEventListener('mouseout', (e) => findxy(e), false);
+  }
+
+  function findxy(e) {
+    let type = e.type;
+    if(type === 'mousedown') {
+      prevX = curX; prevY = curY;
+      curX = e.clientX - canvas.offsetLeft;
+      curY = e.clientY - canvas.offsetTop;
+
+      flag = true; dot_flag = true;
+      if(dot_flag) {
+        context.beginPath();
+        context.fillStyle = foreColor;
+        context.fillRect(curX, curY, 2, 2);
+        context.closePath();
+        dot_flag = false;
+      }
+    } else if(type === 'mouseup' || type === 'mouseout') {
+      flag = false;
+    } else if(type === 'mousemove') {
+      if(flag) {
+        prevX = curX; prevY = curY;
+        curX = e.clientX - canvas.offsetLeft;
+        curY = e.clientY - canvas.offsetTop;
+        draw();
+      }
+    }
+  }
+
+  function draw() {
+    context.beginPath();
+    context.moveTo(prevX, prevY);
+    context.lineTo(curX, curY);
+    context.strokeStyle = foreColor;
+    context.lineWidth = thickness;
+    context.stroke();
+    context.closePath();
   }
 
  return {
@@ -18,4 +80,4 @@ let scribble = (function (window) {
  }
 })(window) 
 
-window.addEventListener('DOMContentLoaded', scribble.init)
+window.addEventListener('DOMContentLoaded', scribble.init);
--- main.js
+++ main.js
@@ -1,63 +1,91 @@
 "use strict"
 
 var main = (function() {
-  const electron = require("electron")
-  const { app, globalShortcut, BrowserWindow, ipcMain, Tray } = require("electron")
-  const notifier = require("node-notifier")
+  const electron = require("electron");
+  const { app, globalShortcut, BrowserWindow, ipcMain, Tray, Menu } = require("electron");
+  const notifier = require("node-notifier");
 
   let tray = null,
   screen = null,
-  scribbleWindow = null
+  scribbleWindow = null;
 
-
-  function setupGlobalShortcuts() {
-    globalShortcut.register("Command+Control+S", toggleScribbleWindow)
+  function initGlobalShortcuts() {
+    globalShortcut.register("Command+Control+S", toggleScribbleWindow);
+    globalShortcut.register("Command+Control+C", clear);
+    globalShortcut.register("Command+Control+H", toggleScribbleWindowHide);
   } 
 
+  function initIpc() {
+    ipcMain.on('clear-done', () => { toggleScribbleWindowHide(), toggleScribbleWindowHide() });
+  }
+
   function setupTray() {
-   tray = new Tray(`${__dirname}/tray.png`)
-   tray.setHighlightMode("never")
+   tray = new Tray(`${__dirname}/tray.png`);
+   tray.setHighlightMode("never");
+   let menuItems = [
+    { label: "Clear", click() { clear() } },
+    { label: "Quit", click() { quit() } }
+   ]
+   tray.setContextMenu(Menu.buildFromTemplate(menuItems))
+  }
+
+  function clear() {
+    if(scribbleWindow) scribbleWindow.webContents.send("clear")
+  }
+
+  function quit() {
+    app.quit()
+  }
+
+  function toggleScribbleWindowHide() {
+    if(!scribbleWindow) return;
+    if(scribbleWindow.isVisible()) {
+      scribbleWindow.hide()
+    } else {
+      scribbleWindow.show() 
+    }
   }
 
   function toggleScribbleWindow() {
     if(scribbleWindow) {
-     scribbleWindow.close()
+     scribbleWindow.close();
     } else {
-      let pointerPosition = screen.getCursorScreenPoint()
-      let display = screen.getDisplayNearestPoint(pointerPosition)
-      let {width, height} = display.workAreaSize
-      notify(width + " " + height)
+      let pointerPosition = screen.getCursorScreenPoint();
+      let display = screen.getDisplayNearestPoint(pointerPosition);
+      let {width, height} = display.workAreaSize;
+      notify(width + " " + height);
       let options = {
         frame: false,
         transparent: true,
         width,
         height
       }
-      scribbleWindow = new BrowserWindow(options)
-      scribbleWindow.on('close', () => scribbleWindow = null)
-      scribbleWindow.loadURL(`file://${__dirname}/app/scribble/main.html`)
+      scribbleWindow = new BrowserWindow(options);
+      scribbleWindow.on('close', () => scribbleWindow = null);
+      scribbleWindow.loadURL(`file://${__dirname}/app/scribble/main.html`);
 
-      if(process.env.debug) scribbleWindow.webContents.openDevTools()
-      scribbleWindow.show()
+      if(process.env.debug) scribbleWindow.webContents.openDevTools();
+      scribbleWindow.show();
     } 
   }
 
   function init() {
     app.on("ready", () => {
       //app.dock.hide()
-      setupGlobalShortcuts()
-      setupTray()
-      app.on("window-all-closed", (e) => e.preventDefault()) 
-      screen = electron.screen
-      toggleScribbleWindow()
+      initGlobalShortcuts();
+      initIpc()
+      setupTray();
+      app.on("window-all-closed", (e) => e.preventDefault()) ;
+      screen = electron.screen;
+      toggleScribbleWindow();
     })
   }
 
   function notify(message) {
-    notifier.notify({title: "Scribble", message})
+    notifier.notify({title: "Scribble", message});
   }
 
-  app.on('will-quit', () => globalShortcut.unregisterAll())
+  app.on('will-quit', () => globalShortcut.unregisterAll());
 
   return {
     init: init
@@ -65,4 +93,4 @@ var main = (function() {
 
 })()
 
-main.init()
+main.init();
